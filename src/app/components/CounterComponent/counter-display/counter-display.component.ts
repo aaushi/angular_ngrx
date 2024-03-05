22ad525a0e842aac268a2057b9474db111c0c9cb
aaushi <aaushi1512@gmail.com>
2024-03-05 19:04:35 +0530

    removing unnecessary subscription

--- src/app/components/CounterComponent/counter-display/counter-display.component.ts
+++ src/app/components/CounterComponent/counter-display/counter-display.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { counterModel } from 'src/app/shared/store/counter.model';
 
 @Component({
@@ -9,18 +9,22 @@ import { counterModel } from 'src/app/shared/store/counter.model';
   styleUrls: ['./counter-display.component.scss']
 })
 export class CounterDisplayComponent implements OnInit {
-  counterDisplay!: number;
+  //counterDisplay!: number;
   constructor(private store: Store<{ counter: counterModel }>) {}
   ownerName = '';
-  counterSubscription!:Subscription
+
+  counterObservable$!:Observable<counterModel>
+  //counterSubscription!:Subscription
   ngOnInit() {
-    console.log(this.counterDisplay);
+    /* console.log(this.counterDisplay);
     this.counterSubscription = this.store.select('counter').subscribe(data => {
       this.counterDisplay = data.counter;
       this.ownerName = data.owner;
-    });
+    });  instead of this we can have only 1 observable and we can subscribe to it in html page*/
+    this.counterObservable$=this.store.select('counter')
   }
   ngOnDestroy(){
-    this.counterSubscription.unsubscribe();
+   // this.counterSubscription.unsubscribe();
+   //since we are not subscribing to anything, we dont need unsubscribe now
   }
 }
